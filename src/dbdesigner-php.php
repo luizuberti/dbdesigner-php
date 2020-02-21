dedf71f679c96bf41eea87852bdf44b1ad54775e
Luiz Uberti <luizuberti@yahoo.com.br>
2020-02-21 14:19:06 -0300

    Correções na estrutura da tabela

--- src/dbdesigner-php.php
+++ src/dbdesigner-php.php
@@ -54,7 +54,6 @@
 				);
 				
 			}
-			//var_dump($relacao); die;
 			
 			$tabela = $xml->METADATA->TABLES->TABLE;
 			$sql_drop=array();
@@ -98,7 +97,7 @@
 					$nome_rel =$nome_tabela."_ibfk_".(++$seq);
 					$idDatatype = $this->tipo_indice((int)$relacao_linha['IndexKind']);
 					$colindex = $dado[(int)$relacao_linha->INDEXCOLUMNS->INDEXCOLUMN['idColumn']][nome];
-					//ALTER TABLE `age_agenda`	       ADD CONSTRAINT `FK_age_agenda_arq_pasta` FOREIGN KEY (`id`) REFERENCES `arq_pasta` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+					
 					$areld[$nome_rel]="ALTER TABLE $nome_tabela DROP FOREIGN KEY $nome_rel;\n";
 					$arel[$nome_rel]="ALTER TABLE $nome_tabela ADD CONSTRAINT $nome_rel FOREIGN KEY(".$relacao[$id][DestCampo].") REFERENCES ".$relacao[$id][SrcTable]."(".$relacao[$id][SrcCampo].") ON DELETE ".$this->tipo_relacionamento($relacao[$id][OnDelete])." ON UPDATE ".$this->tipo_relacionamento($relacao[$id][OnUpdate]).";\n";
 					
@@ -156,8 +155,6 @@
 						$aindd[]="ALTER TABLE $nome_tabela	DROP PRIMARY KEY;\n";
 						$aind[]="ALTER TABLE $nome_tabela	ADD PRIMARY KEY ($colindex);\n";
 						}else{
-						
-						//$aind[$nome_idx]="CREATE INDEX $nome_idx ON $nome_tabela ($colindex);\n";
 						$aindd[$nome_idx]="ALTER TABLE $nome_tabela DROP INDEX $nome_idx;\n";
 						$aind[$nome_idx] ="ALTER TABLE $nome_tabela ADD  INDEX $nome_idx ($colindex);\n";
 					}
@@ -213,17 +210,61 @@
 			
 			$tabela = $xml->METADATA->TABLES->TABLE;
 			$sql_drop=array();
+			$areld=array();
+			$arel=array();
 			$sql="";
 			foreach ($tabela as $tabela_linha){
 				$id   = (int)$tabela_linha['ID'];
-				$nome = (string)$tabela_linha['Tablename'];
+				$nome_tabela = (string)$tabela_linha['Tablename'];
 				$atributo = ((array)$tabela_linha->attributes())["@attributes"];
-				$dado[$id] = array(tipo=>'tabela', nome => $nome , atributo => $atributo );
+				$dado[$id] = array(tipo=>'tabela', nome => $nome_tabela , atributo => $atributo );
+				
 				$StandardInserts=str_replace("\\n","\n",(string)$tabela_linha['StandardInserts']);
+				
+				$TableOptions=str_replace("\\n","\n",(string)$tabela_linha['TableOptions']);
+				
+				$TableOptions = $this->separador($TableOptions);
+				//var_dump($TableOptions);
+				//var_dump($TableOptions[AverageRowLength]);
+				$tabela_detalhe="";
+				if($TableOptions[NextAutoIncVal]!=null && $TableOptions[NextAutoIncVal]>0){
+					$tabela_detalhe.="AUTO_INCREMENT = $TableOptions[NextAutoIncVal]\n";
+				}
+				if($TableOptions[AverageRowLength]!=null && $TableOptions[AverageRowLength]>0){
+					$tabela_detalhe.="AVG_ROW_LENGTH = $TableOptions[AverageRowLength]\n";
+				}
+				if($TableOptions[MaxRowNumber]!=null && $TableOptions[MaxRowNumber]>0){
+					$tabela_detalhe.="MAX_ROWS = $TableOptions[MaxRowNumber]\n";
+				}
+				if($TableOptions[MaxRowNumber]!=null && $TableOptions[MaxRowNumber]>0){
+					$tabela_detalhe.="MIN_ROWS = $TableOptions[MaxRowNumber]\n";
+				}
+				if($TableOptions[PackKeys]!=null && $TableOptions[PackKeys]>0){
+					$tabela_detalhe.="PACK_KEYS = $TableOptions[PackKeys]\n";
+				}
+				if($TableOptions[TblPassword]!=''){
+					$tabela_detalhe.="PASSWORD = '$TableOptions[TblPassword]'\n";
+				}
+				if($TableOptions[DelayKeyTblUpdates]==1){
+					$tabela_detalhe.="DELAY_KEY_WRITE = 1\n";
+				}
+				if($TableOptions[RowChecksum]==1){
+					$tabela_detalhe.="CHECKSUM = 1\n";
+				}
+				if($TableOptions[RowFormat]==1){
+					$tabela_detalhe.="ROW_FORMAT = dynamic\n";
+				}
+				if($TableOptions[RowFormat]==2){
+					$tabela_detalhe.="ROW_FORMAT = fixed\n";
+				}
+				if($TableOptions[RowFormat]==3){
+					$tabela_detalhe.="ROW_FORMAT = compressed\n";
+				}
+				
+				
 				$sql_drop[] = "DROP TABLE IF EXISTS ".(string)$tabela_linha['Tablename'].";";
 				$tabela_comente = (string)$tabela_linha['Comments'];
 				if($tabela_comente != ""){
-					//$tabela_comente = convertletra($tabela_comente);
 					$tabela_comente = "COMMENT '$tabela_comente'";
 				}
 				$sql_tabela ="CREATE TABLE ".(string)$tabela_linha['Tablename']." (\n";
@@ -231,81 +272,81 @@
 				// ***** colunas da tabela
 				$acol=array();
 				foreach($tabela_linha->COLUMNS->COLUMN as $coluna_linha){
-					$id   = (int)$coluna_linha['ID'];
-					$nome = (string)$coluna_linha['ColName'];
-					$atributo = ((array)$coluna_linha->attributes())["@attributes"];
-					
-					$dado[$id] = array(tipo=>'coluna', nome => $nome , atributo => $atributo );
-					
-					$idDatatype = $this->tipo_coluna((int)$coluna_linha['idDatatype']);
-					
-					
-					$DatatypeParams = (string)$coluna_linha['DatatypeParams'];
-					
-					/*se o campo é nulo (1) ou não (0)*/
-					$NotNull = (string)$coluna_linha['NotNull'];
-					if($NotNull == 1){/*atribui as strings aos índices*/
-						$NotNull = "NOT NULL";
-					}
-					else{
-						$NotNull = "NULL";
-					}
-					
-					/*se o campo é auto incremento (1) ou não (0)*/
-					$AutoInc = (string)$coluna_linha['AutoInc'];
-					if($AutoInc == 1){/*atribui as strings aos índices*/
-						$AutoInc = "AUTO_INCREMENT";
-						$autoIncremento = true; /*para verificar a existência de auto incremento no final da tabela*/
-					}
-					else{
-						$AutoInc = "";
-					}
-					
-					/*se o campo é chave primária (1) ou não (0)*/
-					$PrimaryKey = (string)$coluna_linha['PrimaryKey']; 
-					if($PrimaryKey == 1){/*atribui o nome da coluna à chave primária*/
-						$chavePrimaria = $nome; /*agora já sabemos se o campo é chave primária*/
-					}
-					
-					/*se o campo é chave estrangeira (1) ou não (0)*/
-					$IsForeignKey = (string)$coluna_linha['IsForeignKey'];
-					if($IsForeignKey == 1){/*atribui o nome da coluna e do índice à chave estrangeira*/
-						$chaveEstrangeira = $nome;
-					}
-					
-					$Comments = (string)$coluna_linha['Comments'];
-					if($Comments != ""){
-						//$Comments = convertletra($Comments);
-						$Comments = "COMMENT '$Comments'";
-					}
-					
-					/*de posse de todos os dados, monta o sql das colunas*/
-					$acol[]= "  $nome $idDatatype$DatatypeParams $NotNull $AutoInc $Comments";
-					
+				$id   = (int)$coluna_linha['ID'];
+				$nome_coluna = (string)$coluna_linha['ColName'];
+				$atributo = ((array)$coluna_linha->attributes())["@attributes"];
+				
+				$dado[$id] = array(tipo=>'coluna', nome => $nome_coluna , atributo => $atributo );
+				
+				$idDatatype = $this->tipo_coluna((int)$coluna_linha['idDatatype']);
+				
+				
+				$DatatypeParams = (string)$coluna_linha['DatatypeParams'];
+				
+				/*se o campo é nulo (1) ou não (0)*/
+				$NotNull = (string)$coluna_linha['NotNull'];
+				if($NotNull == 1){/*atribui as strings aos índices*/
+					$NotNull = "NOT NULL";
+				}
+				else{
+					$NotNull = "NULL";
+				}
+				
+				/*se o campo é auto incremento (1) ou não (0)*/
+				$AutoInc = (string)$coluna_linha['AutoInc'];
+				if($AutoInc == 1){/*atribui as strings aos índices*/
+					$AutoInc = "AUTO_INCREMENT";
+					$autoIncremento = true; /*para verificar a existência de auto incremento no final da tabela*/
+				}
+				else{
+					$AutoInc = "";
+				}
+				
+				/*se o campo é chave primária (1) ou não (0)*/
+				$PrimaryKey = (string)$coluna_linha['PrimaryKey']; 
+				if($PrimaryKey == 1){/*atribui o nome da coluna à chave primária*/
+					$chavePrimaria = $nome_coluna; /*agora já sabemos se o campo é chave primária*/
+				}
+				
+				/*se o campo é chave estrangeira (1) ou não (0)*/
+				$IsForeignKey = (string)$coluna_linha['IsForeignKey'];
+				if($IsForeignKey == 1){/*atribui o nome da coluna e do índice à chave estrangeira*/
+					$chaveEstrangeira = $nome_coluna;
+				}
+				
+				$Comments = (string)$coluna_linha['Comments'];
+				if($Comments != ""){
+					//$Comments = convertletra($Comments);
+					$Comments = "COMMENT '$Comments'";
+				}
+				
+				/*de posse de todos os dados, monta o sql das colunas*/
+				$acol[]= "  $nome_coluna $idDatatype$DatatypeParams $NotNull $AutoInc $Comments";
+				
 				}
 				
 				// ***** indice da tabela
 				$aind=array();
 				foreach($tabela_linha->INDICES->INDEX as $indice_linha){
 					$id   = (int)$indice_linha['ID'];
-					$nome = (string)$indice_linha['IndexName'];
+					$nome_idx = (string)$indice_linha['IndexName'];
 					$atributo = ((array)$indice_linha->attributes())["@attributes"];
 					
-					$dado[$id] = array(tipo=>'indice', nome => $nome , atributo => $atributo );
+					$dado[$id] = array(tipo=>'indice', nome => $nome_idx , atributo => $atributo );
 					
 					$idDatatype = $this->tipo_indice((int)$indice_linha['IndexKind']);
 					$colindex = $dado[(int)$indice_linha->INDEXCOLUMNS->INDEXCOLUMN['idColumn']][nome];
 					
-					if($nome=='PRIMARY'){
+					if($nome_idx=='PRIMARY'){
 						$aind[]=" $idDatatype ($colindex)";
 						}else{
-						$aind[]=" $idDatatype $nome($colindex)";
+						$aind[]=" $idDatatype $nome_idx($colindex)";
 					}
 					
 				}
 				
 				// ***** Relacionamento da tabela
-				$arel=array();
+				$seq=0;
 				foreach($tabela_linha->RELATIONS_END->RELATION_END as $relacao_linha){
 					$id   = (int)$relacao_linha['ID'];
 					$atributo = ((array)$relacao_linha->attributes())["@attributes"];
@@ -314,20 +355,26 @@
 					
 					$idDatatype = $this->tipo_indice((int)$relacao_linha['IndexKind']);
 					$colindex = $dado[(int)$relacao_linha->INDEXCOLUMNS->INDEXCOLUMN['idColumn']][nome];
-					$arel[]="  FOREIGN KEY(".$relacao[$id][DestCampo].")\n    REFERENCES ".$relacao[$id][SrcTable]."(".$relacao[$id][SrcCampo].")\n      ON DELETE ".$this->tipo_relacionamento($relacao[$id][OnDelete])."\n      ON UPDATE ".$this->tipo_relacionamento($relacao[$id][OnUpdate])."";
+					$nome_rel =$nome_tabela."_ibfk_".(++$seq);
+					$areld[$nome_rel]="ALTER TABLE $nome_tabela DROP FOREIGN KEY $nome_rel;\n";
+					$arel[$nome_rel]="ALTER TABLE $nome_tabela ADD CONSTRAINT $nome_rel FOREIGN KEY(".$relacao[$id][DestCampo].") REFERENCES ".$relacao[$id][SrcTable]."(".$relacao[$id][SrcCampo].") ON DELETE ".$this->tipo_relacionamento($relacao[$id][OnDelete])." ON UPDATE ".$this->tipo_relacionamento($relacao[$id][OnUpdate]).";\n";
 					
 				}
 				
 				$sql_tabela .= implode(",\n",$acol);
 				$sql_tabela .= count($aind)>0 ? ",\n" :"";
 				$sql_tabela .= implode(",\n",$aind);
-				$sql_tabela .= count($arel)>0 ? ",\n" :"";
-				$sql_tabela .= implode(",\n",$arel);
-				$sql_tabela .="\n)ENGINE=InnoDB $tabela_comente;\n";
+				$sql_tabela .="\n)ENGINE=InnoDB";
+				$sql_tabela .= $tabela_detalhe!="" ? "\n$tabela_detalhe" : '';
+				$sql_tabela .= $tabela_comente!="" ? "\n$tabela_comente" : '';
+				$sql_tabela .=";\n";
+				//$sql_tabela .="\n)ENGINE=InnoDB $tabela_comente;\n";
 				$sql_tabela .="$StandardInserts\n";
 				
 				$sql .= $sql_tabela;
 			}
+			//$sql .= implode("",$areld);
+			$sql .= implode("",$arel);
 			$sql_drop=implode("\n\n",$sql_drop);
 			$sql = "SET FOREIGN_KEY_CHECKS=0;\n\n$sql_drop\n\n$sql";
 			$sql = "$sql\nSET FOREIGN_KEY_CHECKS=1;\n";
@@ -390,6 +437,17 @@
 				$str = str_replace($linha[0],"&#$linha[2];",$str);
 			}
 			return html_entity_decode($str);
+		}
+		
+		public function separador($str){
+			$str=explode("\n",$str);
+			$tmp=array();
+			foreach($str as $a => $b){
+				if($b=='') continue;
+				$b=explode('=',$b);
+				$tmp[$b[0]]=$b[1];
+			}
+			return $tmp;
 		}
 	}
 ?>
